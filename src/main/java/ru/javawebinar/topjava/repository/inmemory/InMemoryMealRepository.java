4f5c2fb36765c11c09269e589058cea327902b8b
srgv <>
2021-02-16 11:24:31 +0300

    [HW02 Stage 5] implement MealRestController, MealService

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.inmemory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -13,7 +14,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
     private final Map<Integer, Map<Integer, Meal>> allUsersRepository = new ConcurrentHashMap<>();
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,59 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserCaloriesPerDay;
+import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
 
+    private final MealService service;
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    Meal get(int id) {
+        int userId = authUserId();
+        log.debug("get id={}, userid={}", id, userId);
+        return service.get(id, userId);
+    }
+
+    public void delete(int id) {
+        int userId = authUserId();
+        log.debug("delete id={}, userid={}", id, userId);
+        service.delete(id, userId);
+    }
+
+    public void update(Meal meal, int id) {
+        int userId = authUserId();
+        log.debug("update {}, userid={}", meal, userId);
+        assureIdConsistent(meal, id);
+        service.update(meal, userId);
+    }
+
+    public Meal create(Meal meal) {
+        int userId = authUserId();
+        log.debug("create {}, userid={}", meal, userId);
+        checkNew(meal);
+        return service.create(meal, userId);
+    }
+
+    public List<MealTo> getAll() {
+        int userId = authUserId();
+        log.debug("getall userid={}", userId);
+        return MealsUtil.getTos(service.getAll(userId), authUserCaloriesPerDay());
+    }
 }
